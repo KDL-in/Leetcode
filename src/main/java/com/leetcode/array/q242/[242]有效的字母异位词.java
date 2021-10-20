48693c73a0d1c1b53677b569f2c5b9f0722c25fa
KDL-in <mcwarmsun@hotmail.com>
2021-10-20 08:33:19 +0800

    20211019

---
+++ src/main/java/com/leetcode/array/q242/[242]有效的字母异位词.java
@@ -0,0 +1,50 @@
+package com.leetcode.array.q242;//给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
+//
+// 注意：若 s 和 t 中每个字符出现的次数都相同，则称 s 和 t 互为字母异位词。 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//输入: s = "anagram", t = "nagaram"
+//输出: true
+// 
+//
+// 示例 2: 
+//
+// 
+//输入: s = "rat", t = "car"
+//输出: false 
+//
+// 
+//
+// 提示: 
+//
+// 
+// 1 <= s.length, t.length <= 5 * 10⁴ 
+// s 和 t 仅包含小写字母 
+// 
+//
+// 
+//
+// 进阶: 如果输入字符串包含 unicode 字符怎么办？你能否调整你的解法来应对这种情况？ 
+// Related Topics 哈希表 字符串 排序 👍 441 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+        char[] bucket = new char[256];
+        for (int i = 0; i < s.length(); i++) {
+            bucket[s.charAt(i)]++;
+            bucket[t.charAt(i)]--;
+        }
+        for (int i = 'a'; i <= 'z'; i++) {
+            if (bucket[i] != 0) return false;
+        }
+        return true;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
